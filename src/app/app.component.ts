395dae35659bbed3d1bc57e9177f8a2f224f32f5
kaan.yumak <yumak.kaan@gmail.com>
2024-04-15 16:32:54 +0300

    Upload Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, SharedModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'etiya-fs-angular';
+
+  onButtonClick() {
+    console.log('Button clicked');
+  }
 }
